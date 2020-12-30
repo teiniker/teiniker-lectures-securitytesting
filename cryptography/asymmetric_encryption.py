a9c85de6febdc1012ef91986bd82552cfb7c33f2
teiniker <egon.teiniker@gmail.com>
2020-12-30 10:44:33 -0500

    Initial import.

---
+++ cryptography/asymmetric_encryption.py
@@ -0,0 +1,51 @@
+import unittest
+
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+from cryptography.hazmat.primitives import serialization
+
+# Asymmetric Encryption
+# https://cryptography.io/en/latest/hazmat/primitives/asymmetric/index.html
+# Encryption is performed using the public key, meaning anyone can encrypt data.
+# The data is then decrypted using the private key.
+
+class AsymmetricEncryptionTest(unittest.TestCase):
+
+    def setUp(self):
+        self.private_key = rsa.generate_private_key(public_exponent=65537,key_size=4096)
+        self.public_key = self.private_key.public_key()
+
+
+    def test_encryption_and_decryption(self):
+        message = b"Some data we want to encrypt"
+        print('Message   : ' + message.decode('utf-8'))
+        print('Message   : ' + message.hex())
+
+        # encrypt a message
+        ciphertext = self.public_key.encrypt(
+            message,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        print('Ciphertext: ' + ciphertext.hex())
+
+        # decrypt a message
+        plaintext = self.private_key.decrypt(
+            ciphertext,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        print('Plaintext : ' + plaintext.hex())
+        print('Plaintext : ' + plaintext.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ cryptography/signature.py
@@ -0,0 +1,51 @@
+import unittest
+
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+from cryptography.hazmat.primitives import serialization
+
+# Signature
+# https://cryptography.io/en/latest/hazmat/primitives/asymmetric/index.html
+# A private key can be used to sign a message.
+# This allows anyone with the public key to verify that the message was created
+# by someone who possesses the corresponding private key.
+
+class SignatureTest(unittest.TestCase):
+
+    def setUp(self):
+        self.private_key = rsa.generate_private_key(public_exponent=65537,key_size=4096)
+        self.public_key = self.private_key.public_key()
+
+
+    def test_sign_and_verify(self):
+        message = b"Some data we want to sign"
+        print('Message   : ' + message.decode('utf-8'))
+
+        # singn a message
+        signature = self.private_key.sign(
+            message,
+            padding.PSS(
+                padding.MGF1(
+                    hashes.SHA256()),
+                    salt_length = padding.PSS.MAX_LENGTH
+                ),
+                hashes.SHA256()
+            )
+        print('Signature: ' + signature.hex())
+
+        # verify a message
+        self.public_key.verify(
+            signature,
+            message,
+            padding.PSS(
+                mgf = padding.MGF1(hashes.SHA256()),
+                salt_length = padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        # If the signature does not match, verify() will raise an InvalidSignature exception.
+
+if __name__ == '__main__':
+    unittest.main()
