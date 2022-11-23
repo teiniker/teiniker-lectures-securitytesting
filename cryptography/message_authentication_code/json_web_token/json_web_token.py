0965329ff6b4cb9e6f98ab449911ffba46791dd3
Egon Teiniker <egon.teiniker@gmail.com>
2022-11-23 23:10:58 +0100

    Refactoring.

--- cryptography/message_authentication_code/json_web_token/json_web_token.py
+++ cryptography/message_authentication_code/json_web_token/json_web_token.py
@@ -5,17 +5,17 @@ import jwt
 
 class JsonWebTokenTest(unittest.TestCase):
 
-     def test_secret_gen(self):
+    def test_secret_gen(self):
         self.secret = os.urandom(32).hex()
         print(f'key: {self.secret}')
 
-     def test_encode(self):
+    def test_encode(self):
         payload = {'iss': 'teini', 'sub': '123'}
         secret = '0dc939e138599cfccf6be87df8f7a47d6d79746ce1acd2a4d676f2c40e6e0c3d'
         token = jwt.encode(payload, secret, algorithm='HS256')
         print(f'jwt: {token}')
 
-     def test_decode(self):
+    def test_decode(self):
         token = b'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ0ZWluaSIsInN1YiI6IjEyMyJ9.OOLIGPoPn17dnvn1XH0tfI11t6RJjvDuVXZlYWeerSQ'
         secret = '0dc939e138599cfccf6be87df8f7a47d6d79746ce1acd2a4d676f2c40e6e0c3d'
         payload = jwt.decode(token, secret, algorithms=['HS256'])
