9bcde6c2fecdc080cf7fda23f785a98ac9321c9f
Egon Teiniker <egon.teiniker@gmail.com>
2023-01-26 22:25:49 +0100

    Removed redundant example.

--- penetration-testing/access-control/flask-article-service-tls/article.py
+++
@@ -1,8 +0,0 @@
-
-class ArticleDTO:
-    """Transfer object for Article data"""
-    def __init__(self, oid, description, price) -> None:
-        self.oid = oid
-        self.description = description
-        self.price = price
-
