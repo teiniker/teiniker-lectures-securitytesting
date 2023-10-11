6f540f32e2815aeb6b4f530a4457af0e24d70834
teiniker <egon.teiniker@fh-joanneum.at>
2023-10-11 17:42:26 +0200

    Refactoring.

--- cryptography/random_numbers/unsecure_random_numbers.py
+++ cryptography/random_numbers/unsecure_random_numbers.py
@@ -3,7 +3,7 @@ import random
 
 
 class RandomNumberTest(unittest.TestCase):
-    
+
     def test_randint(self):
         random.seed(1)  # if no value is given, the current system time is used
         b = random.randint(0, 1000)
---
+++ setup/static-analysis/README.md
@@ -0,0 +1,14 @@
+# Static Code Analysis
+
+## Pylint 
+
+```
+$ pip3 install pylint
+```
+
+## Mypy
+
+```
+$ pip3 install mypy
+```
+
