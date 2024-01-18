afb292e6c53c20ac731a744afa47058615c75220
teiniker <egon.teiniker@fh-joanneum.at>
2024-01-18 12:49:27 +0100

    Added file upload example.

---
+++ penetration-testing/datastores/file-handling/flask-upload-service/upload_service.py
@@ -0,0 +1,39 @@
+import os
+import magic
+from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/uploads', methods=['POST'])
+def upload_file():
+    # Check if the post request has the file part
+    if 'file' not in request.files:
+        return jsonify({'message': 'No file part in the request'}), 400
+    file = request.files['file']
+
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        return jsonify({'message': 'No selected file'}), 400
+
+    if file and allowed_file(file.filename):
+        # filename = secure_filename(file.filename)
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return jsonify({'message': f'File {filename} uploaded successfully'}), 200
+
+    return jsonify({'message': 'Allowed file types are png, jpg, jpeg, gif'}), 400
+
+
+if __name__ == '__main__':
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+    app.run(port=8443, debug=True, ssl_context='adhoc')
