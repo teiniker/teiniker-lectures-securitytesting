60fa16d5679a167dc7f2447187176b5d581a6d9e
teiniker <egon.teiniker@gmail.com>
2024-10-30 09:47:20 +0100

    Refactorings.

--- cryptography/message_digest/fingerprint_exercise/fingerprint.py
+++ cryptography/message_digest/fingerprint_exercise/fingerprint.py
@@ -11,24 +11,20 @@ class FingerprintTest(unittest.TestCase):
         self.fingerprint = Fingerprint()
 
     def test_from_bytes(self):
-        data = bytearray(b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f')
-        print(data)
+        data = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
         hash = self.fingerprint.from_bytes(data)
-        print(hash)
         self.assertEqual("be45cb2605bf36bebde684841a28f0fd43c69850a3dce5fedba69928ee3a8991", hash)
 
     def test_from_string(self):
         text = 'Pleased to meet you, hope you guess my name'
         hash = self.fingerprint.from_string(text)
-        print(hash)
         self.assertEqual("3b8be20de1d75d3087f5591b0687d9b3fd036a68b42dc27c5c9618a9bb72732e", hash)
 
     def test_from_file(self):
-        filename = "wordlist.txt"
+        filename = "./tux.jpeg"
         hash = self.fingerprint.from_file(filename)
-        print(hash)
-        self.assertEqual("fd17b0ff31d7fb7b4e202b0cd99e1f11aa1f4972aab81010544a4e562f42bac7", hash)
-
+        self.assertEqual("44eefdc0d4b5ea8d999d743e2651044f8033585ef3c97b2d69b508ad8c282cf1", hash)
+        # compare with $ sha256sum tux.jpeg
 
 if __name__ == '__main__':
     unittest.main()
